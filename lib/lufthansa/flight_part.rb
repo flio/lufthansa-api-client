09ebb9d21ae598d0b17b7e3df44e711e4e6ed39f
kunashir <kunashir@list.ru>
2018-07-06 10:08:53 +0300

    added esitmated time for time status

--- lib/lufthansa/flight_part.rb
+++ lib/lufthansa/flight_part.rb
@@ -5,6 +5,8 @@ module Lufthansa
                   :scheduled_time_utc,
                   :actual_time_local,
                   :actual_time_utc,
+                  :estimated_time_local,
+                  :estimated_time_utc,
                   :time_status,
                   :terminal
 
@@ -24,6 +26,16 @@ module Lufthansa
     def actual_time_utc
       return nil unless @actual_time_utc.is_a?(Hash)
       @actual_time_utc['DateTime']
+    end
+
+    def estimated_time_local
+      return nil unless @estimated_time_local.is_a?(Hash)
+      @estimated_time_local['DateTime']
+    end
+
+    def estimated_time_utc
+      return nil unless @estimated_time_utc.is_a?(Hash)
+      @estimated_time_utc['DateTime']
     end
   end
 end
--- spec/lufthansa/flight_spec.rb
+++ spec/lufthansa/flight_spec.rb
@@ -20,5 +20,43 @@ RSpec.describe Lufthansa::Flight do
         expect(equipment.aircraft_code).to eq 321
       end
     end
+
+    it 'returns estimated time for future flight' do
+      VCR.use_cassette('ew2045_2018-07-06') do
+        flight = Lufthansa::Flight.status('EW2045', "2018-07-06")
+        expect(flight.departure).not_to eq nil
+        expect(flight.arrival).not_to eq nil
+        
+        dep_status = flight.departure.time_status
+        expect(dep_status.definition).to eq 'Flight Delayed'
+        expect(flight.departure.estimated_time_local).not_to eq nil
+
+        dep_terminal = flight.departure.terminal
+        expect(dep_terminal.name).to eq 2
+        expect(dep_terminal.gate).to eq 'A46'
+        
+        equipment = flight.equipment
+        expect(equipment.aircraft_code).to eq '31D'
+      end
+    end
+
+    it 'returns actual time for sent flight' do
+      VCR.use_cassette('ew7464_2018-07-06') do
+        flight = Lufthansa::Flight.status('EW7464', "2018-07-06")
+        expect(flight.departure).not_to eq nil
+        expect(flight.arrival).not_to eq nil
+        
+        dep_status = flight.departure.time_status
+        expect(dep_status.definition).to eq 'Flight Delayed'
+        expect(flight.departure.actual_time_local).not_to eq nil
+
+        dep_terminal = flight.departure.terminal
+        expect(dep_terminal.name).to eq 2
+        expect(dep_terminal.gate).to eq 'B47'
+        
+        equipment = flight.equipment
+        expect(equipment.aircraft_code).to eq '31B'
+      end
+    end
   end
 end
